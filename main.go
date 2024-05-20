1ea4c7495a25ee182fd4766c79025503483f6cbb
BenAnderson72 <Ben.M.Anderson@GMail.com>
2024-05-20 21:34:10 +0000

    1st draft getting and persisting temp

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -11,12 +16,61 @@ import (
 )
 
 type Datax struct {
-	// Desc   string  `json:"desc"`
-	Timestamp time.Time `json:"timestamp"`
-	// Spots     []Spot    `json:"spots"`
+	Temps []Data `json:"temps"`
 }
 
-func Scrape(url string) string {
+type Data struct {
+	Timestamp time.Time `json:"timestamp"`
+	Temp      float64   `json:"temp"`
+}
+
+// The function PersistJSON encodes a given value as JSON and writes it to a file with the specified filename.
+func PersistJSON(filename string, v any) {
+	var buffer bytes.Buffer
+	enc := json.NewEncoder(&buffer)
+	enc.SetIndent("", "    ") // pretty print
+	enc.Encode(&v)
+	fo, _ := os.Create(filename)
+	fo.Write(buffer.Bytes())
+}
+
+func UnmarshalJSON(filename string, v any) {
+	jsonFile, _ := os.Open(filename)
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &v)
+}
+
+func ScrapeTemperatureAndPersist(url string) {
+	out := scrapeTemperatureTrim(url)
+
+	fout, _ := strconv.ParseFloat(out, 32)
+
+	fn := "./temperature.json"
+
+	var dx Datax
+	UnmarshalJSON(fn, &dx)
+
+	d := Data{
+		Timestamp: time.Now(),
+		Temp:      fout,
+	}
+
+	dx.Temps = append(dx.Temps, d)
+
+	PersistJSON(fn, dx)
+
+	// temps := dx.Temps
+
+}
+
+func scrapeTemperatureTrim(url string) string {
+
+	out := scrapeTemperatureFull(url)
+	return strings.ReplaceAll(out, "°C", "")
+}
+
+func scrapeTemperatureFull(url string) string {
 
 	c := colly.NewCollector(colly.Debugger(&debug.LogDebugger{}))
 
@@ -29,110 +83,42 @@ func Scrape(url string) string {
 
 	out := "Nothing"
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
-	})
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Visiting: ", r.URL)
+	// })
 
-	c.OnResponse(func(r *colly.Response) { //get body
-		// fmt.Println("Responding: ", string(r.Body))
-	})
+	// c.OnResponse(func(r *colly.Response) { //get body
+	// 	fmt.Println("Responding: ", string(r.Body))
+	// })
 
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL: ", r.Request.URL, " failed with response: ", r, "\nError: ", err)
 	})
 
+	// https://benjamincongdon.me/blog/2018/03/01/Scraping-the-Web-in-Golang-with-Colly-and-Goquery/
+
+	i := 0
+
 	c.OnHTML("div.flex", func(d *colly.HTMLElement) {
 		if d.Attr("class") == "flex space-x-1 font-normal" {
-			d.ForEach("p.text-sm", func(_ int, p *colly.HTMLElement) {
-				out += p.Text
-			})
-
 			// i := d.DOM.Children().Length()
 			// fmt.Println(i)
+
+			// x := d.DOM.Children()
+
+			d.ForEach("p.text-sm", func(_ int, p *colly.HTMLElement) {
+
+				if i == 1 {
+					out = strings.TrimSpace(p.Text)
+				}
+				i++
+
+			})
 
 		}
 	})
 
-	// c.OnHTML("p.text-sm", func(p *colly.HTMLElement) {
-	// 	out += p.Text
-	// })
-
 	c.Visit(url)
-
-	// p.text-sm
-
-	// c.OnHTML("head title", func(d *colly.HTMLElement) {
-
-	// 	out = d.Text
-
-	// #forecast-day-1 > div > div.TableDayContainer_dayTableWrapper__347n4.MuiBox-root.mui-style-0 > div.MuiTableContainer-root.ForecastTableRows_container__7O5Tf.mui-style-kge0eu > div.ForecastTableRows_containerInner__bwn2U.MuiBox-root.mui-style-0 > div > table:nth-child(1)
-	// d.ForEach("table", func(_ int, t *colly.HTMLElement) {
-
-	// 	//*[@id="forecast-day-1"]/div/div[2]/div[2]/div[1]/div/table[1]/tbody/tr[1]/td[2]
-
-	// 	out = t.Attr("class")
-
-	// 	if t.Attr("class") != "table table-sm table-striped table-inverse table-tide" {
-
-	// 		// mswDays = nil
-
-	// 		d.ForEach("tbody", func(_ int, tb *colly.HTMLElement) {
-
-	// 			tb.ForEach("tr", func(_ int, tr *colly.HTMLElement) {
-
-	// 				if daysIndex > daysToCollect-1 {
-	// 					// break
-	// 				} else if tr.Attr("class") == "tbody-title" {
-	// 				} else if tr.Attr("class") == "background-clear msw-js-tide" {
-	// 				} else {
-	// 					et, e := strconv.ParseInt(tr.Attr("data-timestamp"), 10, 64)
-	// 					if e == nil {
-	// 						t1 := time.Unix(et, 0).In(location)
-
-	// 						// for today skip all earlier forecasts
-	// 						if t1.Day() == time.Now().Day() &&
-	// 							t1.Before(time.Now().Add(-3*time.Hour)) {
-	// 							goto skip
-	// 						}
-
-	// 						// on change of day create the t0 object
-
-	// 						tr.ForEach("td", func(_ int, td *colly.HTMLElement) {
-
-	// 							if strings.HasPrefix(td.Attr("class"), "table-forecast-rating td-nowrap") {
-	// 								class := td.ChildAttrs("li", "class")
-	// 								var counter [2]int
-	// 								// 0 = inactive 1 = active
-	// 								for _, c := range class {
-	// 									switch c {
-	// 									case "active":
-	// 										counter[1]++
-	// 									case "inactive":
-	// 										counter[0]++
-	// 									}
-	// 								}
-	// 								// sw := Swell{Timestamp: t1, StarsNow: [2]int{counter[1], counter[0]}}
-	// 								// swells4day = append(swells4day, sw)
-
-	// 								if !t0.IsZero() && (t1.Day() != t0.Day()) {
-	// 									daysCaught++
-	// 								}
-
-	// 								t0 = t1
-
-	// 							}
-	// 						})
-
-	// 					skip:
-	// 					}
-
-	// 				}
-	// 			})
-	// 		})
-	// 	}
-	// })
-	// })
-	// c.Visit(url)
 
 	return out
 }
--- main_test.go
+++ main_test.go
@@ -1,16 +1,52 @@
 package main
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
-func Test_Scrape(t *testing.T) {
-	Scrape("https://www.thewave.com/live-updates/")
+func Test_ScrapeFull(t *testing.T) {
+
+	act := scrapeTemperatureFull("https://www.thewave.com/live-updates/")
+
+	matched, _ := regexp.MatchString(`^.\d{1,2}\.\d°C$`, act)
+
+	if !matched {
+		t.Errorf("expected something ending with °C, got |%s|", act)
+	}
+
 }
 
-func Test_ScrapeStatic(t *testing.T) {
-	exp := "19.2"
-	act := Scrape("file://./static.html")
+func Test_ScrapeTrim(t *testing.T) {
+
+	act := scrapeTemperatureTrim("https://www.thewave.com/live-updates/")
+
+	matched, _ := regexp.MatchString(`^.\d{1,2}\.\d°C$`, act)
+
+	if matched {
+		t.Errorf("expected something ending without °C, got |%s|", act)
+	}
+
+}
+
+func Test_ScrapeStaticFull(t *testing.T) {
+	exp := "19.3°C"
+	act := scrapeTemperatureFull("file://./static.html")
 
 	if act != exp {
 		t.Errorf("expected %s, got %s", exp, act)
 	}
+}
+
+func Test_ScrapeStaticTrim(t *testing.T) {
+	exp := "19.3"
+	act := scrapeTemperatureTrim("file://./static.html")
+
+	if act != exp {
+		t.Errorf("expected %s, got %s", exp, act)
+	}
+}
+
+func Test_ScrapeTemperatureAndPersist(t *testing.T) {
+	ScrapeTemperatureAndPersist("https://www.thewave.com/live-updates/")
 }
